b54f2acaafba66875c08e7bddbd0856157ddf04b
JorgeMagdaleno <42752137+JorgeMagdaleno@users.noreply.github.com>
2022-03-19 20:42:40 -0700

    Added function to ask images by expositor v2

--- digeek/views.py
+++ digeek/views.py
@@ -60,7 +60,7 @@ class ImagenAApiByExpositor(APIView):
     permission_classes = (IsAuthenticated,)
 
     def get(self, request, id=0):
-        imagen = Imagenes.objects.get(expositor=id)
+        imagen = Imagenes.objects.filter(expositor=id)
         my_serializer = ImagenesSerializer(imagen)
         return JsonResponse(my_serializer.data, safe=False)
 
