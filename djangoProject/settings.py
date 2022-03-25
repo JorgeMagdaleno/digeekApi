8acc6c7da1cdb828b338233846c6ad546057abcf
JorgeMagdaleno <42752137+JorgeMagdaleno@users.noreply.github.com>
2022-03-25 14:34:35 -0700

    Images test 6

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -131,7 +131,7 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
