56f95b55c1baef4e92ad7c04a48bb51994c9e5d0
JorgeMagdaleno <42752137+JorgeMagdaleno@users.noreply.github.com>
2022-03-25 12:25:11 -0700

    Fixed bug

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include, re_path
 from rest_framework_simplejwt import views as jwt_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^',include('digeek.urls')),
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
