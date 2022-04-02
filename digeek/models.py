4ea58d60c2e87ee50efd42f9236b16ba0e866252
JorgeMagdaleno <42752137+JorgeMagdaleno@users.noreply.github.com>
2022-04-01 20:29:33 -0700

    Coreccion Vista Visitante

--- digeek/models.py
+++ digeek/models.py
@@ -101,6 +101,7 @@ class Visitante(models.Model):
     url_comprobante_pago = models.CharField(max_length=255, blank=True, null=True)
     last_update = models.DateTimeField()
     url_imagen = models.ImageField(upload_to=upload_to, blank=True, null=True, default='imagen/default.jpg')
+    email = models.CharField(max_length=100)
 
     class Meta:
         managed = False
